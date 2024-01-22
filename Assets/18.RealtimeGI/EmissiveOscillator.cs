8afc44f307ec28feacf7d51d793fe2394ac3d1c7
Brian6030 <brian2009122@gmail.com>
2024-01-22 18:33:36 +0900

    18.RealtimeGI (GI part)

---
+++ Assets/18.RealtimeGI/EmissiveOscillator.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmissiveOscillator : MonoBehaviour
+{
+    MeshRenderer emissiveRenderer;
+    Material emissiveMaterial;
+    void Start()
+    {
+        emissiveRenderer = GetComponent<MeshRenderer>();
+        emissiveMaterial = emissiveRenderer.material;
+    }
+
+    void Update()
+    {
+        Color c = Color.Lerp(Color.blue, Color.black, Mathf.Sin(Time.time * Mathf.PI) * 0.5f + 0.5f);
+        emissiveMaterial.SetColor("_Emission", c);
+        DynamicGI.SetEmissive(emissiveRenderer, c);
+    }
+}
